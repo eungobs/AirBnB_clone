a134d1c1bea7e8f176b630f8d8928b7362ced787
hectoriabandini <abuhmoseji@gmail.com>
2023-10-12 11:44:41 -0700

    user

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines the User class."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Creates a user.
+
+    Attributes:
+        email (str): The email of each user.
+        password (str): The password of each user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
