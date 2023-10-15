9013d53c0fd19f808c1eae4ad61c868bb64debdd
hectoriabandini <johnyREx@gmail.com>
2023-10-15 12:25:04 -0700

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,26 +1,58 @@
 #!/usr/bin/python3
 
-"""
-Defines a unittest for consol.py.
-"""
+"""Defines a unittests for console.py"""
 import unittest
 from io import StringIO
+from unittest.mock import patch
 from console import HBNBCommand
 
-class TestConsole(unittest.TestCase):
-    def test_quit(self):
-        """Test that 'quit' command exits the program"""
-        with unittest.mock.patch('sys.stdout', new_callable=StringIO):
-            with unittest.mock.patch('sys.stdin', StringIO('quit\n')):
-                console = HBNBCommand()
-                self.assertTrue(console.onecmd('quit'))
+class TestHBNBConsole(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.console = HBNBCommand()
 
-    def test_EOF(self):
-        """Test that 'EOF' command exits the program"""
-        with unittest.mock.patch('sys.stdout', new_callable=StringIO):
-            with unittest.mock.patch('sys.stdin', StringIO('EOF\n')):
-                console = HBNBCommand()
-                self.assertTrue(console.onecmd('EOF'))
+    def setUp(self):
+        self.mock_stdout = StringIO()
+
+    def tearDown(self):
+        self.mock_stdout.close()
+
+    def test_create(self):
+        with patch('sys.stdout', new=self.mock_stdout):
+            self.console.onecmd("create User")
+        output = self.mock_stdout.getvalue()
+        self.assertTrue("User" in output)
+
+    def test_show(self):
+        with patch('sys.stdout', new=self.mock_stdout):
+            self.console.onecmd("show User 1234-5678")
+        output = self.mock_stdout.getvalue()
+        self.assertTrue("User" in output)
+
+    def test_destroy(self):
+        with patch('sys.stdout', new=self.mock_stdout):
+            self.console.onecmd("destroy User 1234-5678")
+        output = self.mock_stdout.getvalue()
+        self.assertTrue("User" in output)
+
+    def test_all(self):
+        with patch('sys.stdout', new=self.mock_stdout):
+            self.console.onecmd("all User")
+        output = self.mock_stdout.getvalue()
+        self.assertTrue("User" in output)
+
+    def test_count(self):
+        with patch('sys.stdout', new=self.mock_stdout):
+            self.console.onecmd("count User")
+        output = self.mock_stdout.getvalue()
+        self.assertTrue("User" in output)
+
+    def test_update(self):
+        with patch('sys.stdout', new=self.mock_stdout):
+            self.console.onecmd("User.update(1234-5678, {'email': 'newemail@example.com'})")
+        output = self.mock_stdout.getvalue()
+        self.assertTrue("User" in output)
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
+ 
