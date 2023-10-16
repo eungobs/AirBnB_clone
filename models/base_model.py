e26d9deefe5eae7e4e6d0dc39582fbaf9e2cf2c1
hectoriabandini <johnyREx@gmail.com>
2023-10-16 13:19:57 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""Defines class"""
 import uuid
-from datetime import datetime
+import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-    
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
-    def save(self):
-        self.updated_at = datetime.now()
-    
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        val = datetime.datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
+    def update_timestamp(self):
+        self.updated_at = datetime.datetime.now()
+
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
+
+    def save(self):
+        self.update_timestamp()
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        gname = self.__class__.__name__
+        return "[{}] ({}) {}".format(gname, self.id, self.__dict__)
+~
