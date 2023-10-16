c46976c165de649661d54acdd7ec25f646b5d0bf
hectoriabandini <abuhmoseji@gmail.com>
2023-10-16 14:18:07 -0700

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 import uuid
-import datetime
 from models import storage
-"""my basemodel"""
+from datetime import datetime
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
-            for key, val in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        val = datetime.datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, val)
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-
-    def update_timestamp(self):
-        self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = self.create_at
 
     def to_dict(self):
         data = self.__dict__.copy()
