26708cb9edfcfe01e2cdca09ab49c909d24478dc
hectoriabandini <abuhmoseji@gmail.com>
2023-10-16 17:21:28 -0700

    still based

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,30 @@
 import models
 from uuid import uuid4
 from datetime import datetime
-from models.engine.file_storage import FileStorage
 
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            FileStorage().new(self)
+            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
